1ed0f62da3c6930568a079dd40390c0500ccab2f
Ubuntu <ubuntu@ip-172-31-6-146.ca-central-1.compute.internal>
2020-11-02 03:39:06 +0000

    ft 400

--- encoder/train.py
+++ encoder/train.py
@@ -132,7 +132,7 @@ def train(run_id: str, clean_data_root: Path, models_dir: Path, umap_every: int,
         
         n += 1
         # exit after 20 loop if fine tuning is on
-        if n > 200:
+        if n > 400:
             print("fine tuning finished!")
             break
 
